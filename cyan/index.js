d5c2d32f42c998a0c08589713f2d318f52eada87
kirinnee <kirinnee97@gmail.com>
2023-11-14 10:58:17 +0800

    feat: initial commit

---
+++ cyan/index.js
@@ -0,0 +1,46 @@
+import { CyanGlob, GlobType, QuestionType, StartTemplateWithLambda } from "@atomicloud/cyan-sdk";
+
+StartTemplateWithLambda(async (i, d) => {
+  const name = await i.text("What is your name?");
+
+  const age = await i.text({
+    message: "How old are you?",
+    type: QuestionType.Text,
+    validate: (v) => {
+      try {
+        const n = parseInt(v)
+        if (n < 19) {
+          return "Age needs to be large than 19"
+        }
+        if (n > 150)
+          return "Age needs to be less than 150"
+        return ""
+      } catch {
+        return "Please enter a number"
+      }
+    },
+  });
+
+
+  return {
+    processors: [
+      {
+        name: "kirinnee/dotnet-handlebar",
+        files: [
+          {
+            glob: "**/*.*",
+            type: GlobType.Template,
+            exclude: ["scripts", ".github", "cyan.yaml"],
+          },
+        ],
+        config: {
+          vars: {
+            name,
+            age,
+          }
+        },
+      },
+    ],
+    plugins: [],
+  }
+});
